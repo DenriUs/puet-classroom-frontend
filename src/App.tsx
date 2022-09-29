5312354d399ecea8f171d20180ed76f85def3dcc
DenriUs <den.tacij@gmail.com>
2022-09-29 23:29:58 +0300

    feat(auth): complete login modal view

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import LoginModal from './components/modals/login/Login';
+import LoginModal from './components/modals/Login';
 
 import './global/sass/style.scss';
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <LoginModal />
     </div>
   );
--- src/components/modals/login/Login.tsx
+++ src/components/modals/login/Login.tsx
@@ -1,17 +1,31 @@
-import puetLogo from '../../../../public/puetLogo.png';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import puetLogo from '../../../assets/puetLogo.png';
 
 import './Login.scss';
 
 const LoginModal = () => {
   return (
     <>
-      <div className="login-modal">
-        <div className="login-modal__left-side">
-          <img src={puetLogo} />
+      <div className='login-modal'>
+        <div className='login-modal__left-side'>
+          <img className='left-side__logo' src={puetLogo} />
+          <p className='left-side__logo-name'>Classroom</p>
         </div>
-        <div className="login-modal__right-side"></div>
+        <div className='login-modal__right-side'>
+          <div className='login-form'>
+            <div className='header'>
+              <p>Вхід</p>
+            </div>
+            <TextField className='email-input' label='Email' variant='standard' />
+            <TextField className='password-input' label='Password' variant='standard' />
+            <Button className='submit-button' variant='contained' size='large'>
+              Увійти
+            </Button>
+          </div>
+        </div>
       </div>
-      <div className="modal-overlay--login"></div>
+      <div className='modal-overlay--login'></div>
     </>
   );
 };
