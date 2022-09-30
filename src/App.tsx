031900e716d36014dc6ced18e159e7c6d92d2a3d
Illya <illya-komar@ukr.net>
2022-09-30 08:48:35 +0300

    feat(layout): add sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import LoginModal from './components/modals/login/Login';
+import Layout from './pages/Layout/Layout';
 
 import './global/sass/style.scss';
 
 function App() {
   return (
     <div className="App">
-      <LoginModal />
+      <Layout />
     </div>
   );
 }
