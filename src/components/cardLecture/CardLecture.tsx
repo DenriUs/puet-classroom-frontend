8760618440c0eaedea2f872fd1cdddbf785e7ed8
Illya <illya-komar@ukr.net>
2022-10-06 16:39:01 +0300

    feat(leyout): add course header and course main

---
+++ src/components/cardLecture/CardLecture.tsx
@@ -0,0 +1,40 @@
+import './CardLecture.scss';
+import { FilePdfOutlined } from '@ant-design/icons';
+
+interface Props {}
+
+const CardLecture = (props: Props) => {
+  return (
+    <div className="box">
+      <div className="box-card">
+        <div className="box-card__title-lecture">Тема 1. Базові поняття інформаційних мереж</div>
+        <div className="box-card__file">
+          <div className="name-file">Базові поняття інформаційних мереж.pptx</div>
+          <div className="icon-file">
+            <FilePdfOutlined />
+          </div>
+        </div>
+        <div className="box-card__file">
+          <div className="name-file">Базові поняття інформаційних мереж.pptx</div>
+          <div className="icon-file">
+            <FilePdfOutlined />
+          </div>
+        </div>
+        <div className="box-card__file">
+          <div className="name-file">Базові поняття інформаційних мереж.pptx</div>
+          <div className="icon-file">
+            <FilePdfOutlined />
+          </div>
+        </div>
+        <div className="box-card__file">
+          <div className="name-file">Базові поняття інформаційних мереж.pptx</div>
+          <div className="icon-file">
+            <FilePdfOutlined />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CardLecture;
---
+++ src/components/practicalLecture/PracticalLecture.tsx
@@ -0,0 +1,39 @@
+import './PracticalLecture.scss';
+import { FilePdfOutlined } from '@ant-design/icons';
+import { Button } from 'antd';
+import { useState } from 'react';
+import Practical from '../modals/practical/Practical';
+
+interface Props {}
+
+const PracticalLecture = (props: Props) => {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <div className="box">
+      <div className="box-card">
+        <div className="box-card__title-lecture">
+          Практична робота 1. Базові поняття інформаційних мереж
+        </div>
+        <div className="box-card__file">
+          <div className="name-file">Інформаційних мереж.pdf</div>
+          <div className="icon-file">
+            <FilePdfOutlined />
+          </div>
+        </div>
+        <div className="mark">Оцінка 0,00 / 4,00</div>
+        <div>
+          <Button shape="round" className="button-pass" onClick={handleShow}>
+            Здати роботу
+          </Button>
+          <Practical onStart={show} handleClose={handleClose} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PracticalLecture;
