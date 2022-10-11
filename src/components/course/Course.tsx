337d304fcbb5d72891db13ae07edfe9845f8c707
Illya <illya-komar@ukr.net>
2022-10-11 08:20:03 +0300

    feat(layout): delete course

--- src/components/course/Course.tsx
+++ src/components/course/Course.tsx
@@ -1,5 +1,11 @@
-import { Button } from 'antd';
-import { PlusCircleFilled } from '@ant-design/icons';
+import { Button, Dropdown, Menu, Modal } from 'antd';
+import {
+  PlusCircleFilled,
+  MoreOutlined,
+  EditOutlined,
+  DeleteOutlined,
+  ExclamationCircleOutlined,
+} from '@ant-design/icons';
 import { useState } from 'react';
 
 import './Course.scss';
@@ -7,7 +13,50 @@ import './Course.scss';
 import profileIcon from '../../assets/profileIcon.png';
 import CourseModal from '../modals/course/Course';
 
+const { confirm } = Modal;
+
 interface Props {}
+
+const showDeleteConfirm = () => {
+  confirm({
+    title: 'Ви дійсно бажаєте видалити курс ?',
+    icon: <ExclamationCircleOutlined />,
+    okText: 'Так',
+    okType: 'danger',
+    cancelText: 'Ні',
+    onOk() {
+      console.log('ok');
+    },
+    onCancel() {
+      console.log('ok');
+    },
+  });
+};
+
+const menu = (
+  <Menu
+    items={[
+      {
+        label: (
+          <div className='dropdown-name'>
+            <EditOutlined className='icon-edit' />
+            Редагувати
+          </div>
+        ),
+        key: '1',
+      },
+      {
+        label: (
+          <div className='dropdown-name' onClick={showDeleteConfirm}>
+            <DeleteOutlined className='icon-delete' />
+            Видалити
+          </div>
+        ),
+        key: '2',
+      },
+    ]}
+  />
+);
 
 const Course = (props: Props) => {
   const [show, setShow] = useState(false);
@@ -32,6 +81,16 @@ const Course = (props: Props) => {
       </div>
       <div className='course-card'>
         <div className='course-card__container'>
+          <div className='dropdown-container'>
+            <Dropdown
+              overlay={menu}
+              placement='bottomLeft'
+              className='dropdown-item'
+              trigger={['click']}
+            >
+              <MoreOutlined className='icon-more' />
+            </Dropdown>
+          </div>
           <span className='title'>Хімія</span>
           <div className='image'>
             <img src={profileIcon} alt='icon' />
@@ -42,6 +101,16 @@ const Course = (props: Props) => {
           </div>
         </div>
         <div className='course-card__container'>
+          <div className='dropdown-container'>
+            <Dropdown
+              overlay={menu}
+              placement='bottomLeft'
+              className='dropdown-item'
+              trigger={['click']}
+            >
+              <MoreOutlined className='icon-more' />
+            </Dropdown>
+          </div>
           <span className='title'>Хімія</span>
           <div className='image'>
             <img src={profileIcon} alt='icon' />
@@ -52,6 +121,16 @@ const Course = (props: Props) => {
           </div>
         </div>
         <div className='course-card__container'>
+          <div className='dropdown-container'>
+            <Dropdown
+              overlay={menu}
+              placement='bottomLeft'
+              className='dropdown-item'
+              trigger={['click']}
+            >
+              <MoreOutlined className='icon-more' />
+            </Dropdown>
+          </div>
           <span className='title'>Хімія</span>
           <div className='image'>
             <img src={profileIcon} alt='icon' />
@@ -62,6 +141,16 @@ const Course = (props: Props) => {
           </div>
         </div>
         <div className='course-card__container'>
+          <div className='dropdown-container'>
+            <Dropdown
+              overlay={menu}
+              placement='bottomLeft'
+              className='dropdown-item'
+              trigger={['click']}
+            >
+              <MoreOutlined className='icon-more' />
+            </Dropdown>
+          </div>
           <span className='title'>Хімія</span>
           <div className='image'>
             <img src={profileIcon} alt='icon' />
@@ -72,6 +161,16 @@ const Course = (props: Props) => {
           </div>
         </div>
         <div className='course-card__container'>
+          <div className='dropdown-container'>
+            <Dropdown
+              overlay={menu}
+              placement='bottomLeft'
+              className='dropdown-item'
+              trigger={['click']}
+            >
+              <MoreOutlined className='icon-more' />
+            </Dropdown>
+          </div>
           <span className='title'>Хімія</span>
           <div className='image'>
             <img src={profileIcon} alt='icon' />
