541c1ba291ab61408c6d1cb2b2ed9b89607ad154
Ivan Shapovalov <sluvkann@gmail.com>
2023-06-12 00:15:53 +0200

    chore: change port

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     open: true,
+    port: 3001,
   },
   css: {
     preprocessorOptions: {
